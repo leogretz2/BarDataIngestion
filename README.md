22024082b2bdeeaacf8056af6fc53dde94c9b265
Leo Gretzinger <leogretz@gmail.com>
2024-05-12 22:48:31 +0000

    lkj

---
+++ README.md
@@ -0,0 +1,7 @@
+Notes on formats for pdf/docx/txt files:
+
+- 300_MBE_Q&As.pdf: Page # then Question then Answer 300x
+- Barbri_Released_Questions_MBE_2007.pdf - images so needs OCR - 
+- Civil_notes.pdf - images so needs OCR, two pages of notes, so probably useless
+- Con_Law_Adaptibar.pdf - past questions; one question then explanations for each option per page; Question 8-1588, many skipped
+- Contract_Adaptibar.pdf - past questions; one question then explanations for each option per page; Question 5-1593, many skipped
--- extract_txt_miner.py
+++ extract_txt_miner.py
@@ -1,5 +1,5 @@
 from pdfminer.high_level import extract_text
-
+import docx
 
 def extract_text_from_pdf(pdf_path):
   try:
@@ -8,15 +8,33 @@ def extract_text_from_pdf(pdf_path):
   except Exception as e:
     return f"Error extracting text: {str(e)}"
 
+def extract_text_from_docx(file_path):
+    doc = docx.Document(file_path)
+    return "\n".join([paragraph.text for paragraph in doc.paragraphs])
+
 
 # Usage:
-pdf_path_question = './working_folder/NCBE_Online_MBE_Practice_Exam_4/NCBE_Online_MBE_Practice_Exam_4_Questions.pdf'
-pdf_path_answer = './working_folder/NCBE_Online_MBE_Practice_Exam_4/NCBE_Online_MBE_Practice_Exam_4_Answers.pdf'
-extracted_text_question = extract_text_from_pdf(pdf_path_question)
-extracted_text_answer = extract_text_from_pdf(pdf_path_answer)
-print(extracted_text_question[:100])
-txt_path = './txt_folder/NCBE_Online_MBE_Practice_Exam_4.txt'
+if False:
+  # folder, two files
+  docx_path_question = './working_folder/Torts/Torts_Questions.docx' 
+  docx_path_answer = './working_folder/Torts/Torts_Answers.docx' 
+  extracted_text_question = extract_text_from_docx(docx_path_question)
+  extracted_text_answer = extract_text_from_docx(docx_path_answer)
+  #pdf_path_question = './working_folder/NCBE_Online_MBE_Practice_Exam_4/NCBE_Online_MBE_Practice_Exam_4_Questions.pdf'
+  #pdf_path_answer = './working_folder/NCBE_Online_MBE_Practice_Exam_4/NCBE_Online_MBE_Practice_Exam_4_Answers.pdf'
+  # extracted_text_question = extract_text_from_pdf(pdf_path_question)
+  # extracted_text_answer = extract_text_from_pdf(pdf_path_answer)
+  print('q: ',extracted_text_question[:100], '\n', extracted_text_question[-100:], '\n\na: ', extracted_text_answer[:100],'\n', extracted_text_answer[-100:])
+  txt_path = './txt_folder/Torts.txt'
+else:
+  # one-off file
+  pdf_path = './working_folder/Contracts_Adaptibar.pdf'
+  extracted_text = extract_text_from_pdf(pdf_path)
+  print(extracted_text[:100], '\n', extracted_text[-100:])
 
+  txt_path = './txt_folder/Contracts_Adaptibar.txt'
+  
 with open(txt_path, 'w') as f:
-  f.write(extracted_text_question+extracted_text_answer)
+  f.write(extracted_text)
   print(f"Combined text saved to {txt_path}")
+  
